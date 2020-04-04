fd02c4024e0e01d79eaef6cb358413bbd43a8f2e
Matt Bentley <mbentley@mbentley.net>
2020-04-04 07:32:03 -0400

    Updates to the md2rst script
    Signed-off-by: Matt Bentley <mbentley@mbentley.net>

--- md2rst.sh
+++ md2rst.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-#set -x
-
 if [ $# != 2 ]; then
-    echo "error: $0 requires 2 arguments"
-    echo "usage:"
-    echo " $(basename "$0") <source dir> <destination dir>"
-    exit 1
+  echo "error: $0 requires 2 arguments"
+  echo "usage:"
+  echo " $(basename "$0") <source dir> <destination dir>"
+  exit 1
 fi
 
 SRC="$1"
@@ -14,27 +12,25 @@ DEST="$2"
 export SRC DEST
 
 if [ ! -d "$SRC" ]; then
-    echo "error: source '$SRC' is not a directory"
-    exit 1
+  echo "error: source '$SRC' is not a directory"
+  exit 1
 fi
 
 if ! mkdir -p "$DEST"; then
-    echo "error: failed to create destination '$DEST'"
-    exit 1
+  echo "error: failed to create destination '$DEST'"
+  exit 1
 fi
 
 convfunc() {
-    input="$1"
-    inputdir=$(dirname "$input")
-    destdir="$DEST/$inputdir"
-    mkdir -p "$destdir"
-    destfile="$DEST/${input%.md}.rst"
-    echo "$input -> $destfile"
-    pandoc -o "$destfile" "$input"
+  input="$1"
+  inputdir=$(dirname "$input")
+  destdir="$DEST/$inputdir"
+  mkdir -p "$destdir"
+  destfile="$DEST/${input%.md}.rst"
+  echo "$input -> $destfile"
+  pandoc -o "$destfile" "$input"
 }
 export -f convfunc
 
-cd "$SRC" || exit
-
-find . -iname '*.md' -print0 | xargs -0 -n1 bash -c 'convfunc "$@"' arg0
+find "$SRC" -iname '*.md' -print0 | xargs -0 -n1 bash -c 'convfunc "$@"' arg0
 echo "Done."
